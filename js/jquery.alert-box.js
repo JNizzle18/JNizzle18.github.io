f658debc01b163596022691d342d209428f27fe1
JNizzle18 <40846959+JNizzle18@users.noreply.github.com>
2018-07-05 15:41:38 +1000

    Test1

---
+++ js/jquery.alert-box.js
@@ -0,0 +1,18 @@
+(function($) {
+  $.fn.AlertBox = function(options) {
+
+    // default settings:
+    var defaults = {
+      sumbitSelector = "#submit",
+      cancelSelector = "#cancel",
+      callback = null
+    };
+
+    var settings = $.extend({}, defaults, options);
+
+    // return this.each(function() {
+    // Plugin code would go here...
+    // });
+
+  };
+}(jQuery));
